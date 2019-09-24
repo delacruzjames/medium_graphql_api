e64d23cd5070379dbeaf651246ef08b207952725
James Dela Cruz <delacruzjamesmartin@gmail.com>
2019-09-24 20:56:58 +0800

    Added absinthe

--- mix.exs
+++ mix.exs
@@ -41,7 +41,10 @@ defmodule MediumGraphqlApi.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:argon2_elixir, "~> 2.0"}
+      {:argon2_elixir, "~> 2.0"},
+      {:absinthe, "~> 1.4.0"},
+      {:absinthe_plug, "~> 1.4"},
+      {:absinthe_ecto, "~> 0.1.3"}
     ]
   end
 
