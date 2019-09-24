87db4c2201d7253a19a095bf256f45b3e48ce2f6
James Dela Cruz <delacruzjamesmartin@gmail.com>
2019-09-24 18:20:18 +0800

    run formatter

--- test/medium_graphql_api_web/views/error_view_test.exs
+++ test/medium_graphql_api_web/views/error_view_test.exs
@@ -5,7 +5,9 @@ defmodule MediumGraphqlApiWeb.ErrorViewTest do
   import Phoenix.View
 
   test "renders 404.json" do
-    assert render(MediumGraphqlApiWeb.ErrorView, "404.json", []) == %{errors: %{detail: "Not Found"}}
+    assert render(MediumGraphqlApiWeb.ErrorView, "404.json", []) == %{
+             errors: %{detail: "Not Found"}
+           }
   end
 
   test "renders 500.json" do
